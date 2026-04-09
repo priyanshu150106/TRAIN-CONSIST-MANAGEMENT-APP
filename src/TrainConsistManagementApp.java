31bbcff00dd9d039be0af3a5b5344c52030953ea
priyanshu150106 <priyanshu150106@gmail.com>
2026-04-09 14:44:01 +0530

    UC8: Filter Passenger Bogies Using Streams

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,1 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class TrainConsistManagementApp {
+
+    public static void main(String[] args) {
+
+        String trainName = "Express";
+        String engine = "WAP-7";
+
+        List<String> bogies = new ArrayList<>();
+
+        bogies.add("S1");
+        bogies.add("S2");
+        bogies.add("A1");
+        bogies.add("GEN1");
+        bogies.add("S3");
+
+        System.out.println("All Bogies: " + bogies);
+
+        // 🔍 Filter only passenger bogies (Sleeper = S)
+        List<String> passengerBogies = bogies.stream()
+                .filter(b -> b.startsWith("S"))
+                .collect(Collectors.toList());
+
+        System.out.println("Passenger Bogies: " + passengerBogies);
+    }
+}
